87729c48131ea9d57b6be3459bed55747ddd92c5
Vincent <Vincentnguyen030@gmail.com>
2024-05-18 14:54:29 -0500

    Add book create post form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -76,7 +76,7 @@ exports.author_create_post = [
 
     if (!errors.isEmpty()) {
       // There are errors. Render form again with sanitized values/errors messages.
-      res.render("author_form", {
+      res.render("layout", {
         title: "Create Author",
         author: author,
         errors: errors.array(),
